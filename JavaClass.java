d19d597f39944c35ec15e694bb8a14e997106186
Abdur Rahman <e241018@ugrad.iiuc.ac.bd>
2024-11-08 22:42:18 -0500

    Added by me

---
+++ JavaClass.java
@@ -0,0 +1,21 @@
+package BeginnerJava;
+
+public class JavaClass {
+
+	public static void main(String[] args) {
+		boolean b=true;
+		char c = 'a';
+		short s=1234;
+		int i=3443;
+		float f=10.2f;
+		double d=16.7;
+		
+	System.out.printf("boolean b = %b\n",b);
+	System.out.printf("char c=%c\n",c);
+	System.out.printf("short s=%s\n",s);
+	System.out.printf("int i=%d\n",i);
+	System.out.printf("float f=%.1f\n",f);
+	System.out.printf("double d=%.3f\n",d);
+	}
+
+}
---
+++ Name.java
@@ -0,0 +1,9 @@
+package NewJava;
+
+public class Name {
+
+	public static void main(String[] args) {
+		System.out.print("Abdur Rahman");
+	}
+
+}
