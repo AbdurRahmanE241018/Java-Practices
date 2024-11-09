9dd3ee439122fee4ad25d07cbe7cdb5f1abdaccc
Abdur Rahman <e241018@ugrad.iiuc.ac.bd>
2024-11-08 22:47:29 -0500

    Added by me

---
+++ BioData.java
@@ -0,0 +1,11 @@
+
+package javaapplication2;
+
+public class BioData {
+    
+    public static void main(String [] args){
+        
+        System.out.println("Akash");
+        System.out.println("01842776586");
+    }
+}
