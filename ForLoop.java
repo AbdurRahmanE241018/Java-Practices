bb090d6ccb6b996a27fa4ee670a4f63433b48fd4
Abdur Rahman <e241018@ugrad.iiuc.ac.bd>
2024-11-09 12:10:44 -0500

    Loop Practice

---
+++ ForLoop.java
@@ -0,0 +1,23 @@
+package Loop;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+		for (int i=1; i<=10;i++) {
+		//	System.out.println("Bangladesh");
+		//	System.out.println(i); /* 1 2 3 4 ...10 */
+			
+			System.out.println(i+". Bangladesh");
+			
+			/*1.Bangladesh
+			2.Bangladesh
+			3.Bangladesh
+			.......
+			10.Bangladesh
+			*/
+						
+		}
+	}
+
+}
---
+++ NumberForLoop.java
@@ -0,0 +1,22 @@
+package Loop;
+
+public class NumberForLoop {
+
+	public static void main(String[] args) {
+		
+		for (int i=2; i<=50;i=i+2) //Increment 
+		{ 
+			System.out.println(i);
+		}
+		
+		/*
+		for (int i=50; i>=2;i=i-2) //Decrement 
+		{ 
+			System.out.println(i);
+		}
+		*/
+
+
+	}
+
+}
