421d6788d25a8f10d401dac35cafcbe19072eaca
Abdur Rahman <e241018@ugrad.iiuc.ac.bd>
2024-11-16 12:16:48 -0500

    Method Overloaading

---
+++ Overloading_Test.java
@@ -0,0 +1,13 @@
+
+package Method_Overloading;
+
+public class Overloading_Test {
+    
+    public static void main(String[] args) {
+        Overloading ob = new Overloading();
+        ob.add(5,10);
+        ob.add(6.5,5.9);
+        ob.add(2, 8, 12);
+        ob.add();
+    }
+}
