d5cfe15958b2f0024720461dbc861799413f259c
Abdur Rahman <e241018@ugrad.iiuc.ac.bd>
2024-11-16 12:24:23 -0500

    Break

---
+++ BreakJava.java
@@ -0,0 +1,11 @@
+package Break;
+public class BreakJava {
+    public static void main(String args[]){
+      for(int i=1;i<10;i++){
+    if (i!=5) {
+    } else {
+    continue;}
+    System.out.print(i);  
+}
+}
+}
