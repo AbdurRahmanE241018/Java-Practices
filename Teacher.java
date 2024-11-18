3ceedd24f3635fa9a7b1532f32511952f77d5d57
Abdur Rahman <e241018@ugrad.iiuc.ac.bd>
2024-11-18 06:17:07 -0500

    Usage of Parametre

--- Teacher.java
+++ Teacher.java
@@ -1,7 +1,19 @@
 
-package OOP;
+package OOP.Parametrised_Method;
 
 public class Teacher {
     String name,gender;
     int phone,id;
+    void setInformation(String n,String m,int ph){
+        name = n;
+        gender = m;
+        phone = ph;
+    }
+    void displayInformation(){
+        System.out.println("Name:" + name);
+        System.out.println("Gender:" + gender);
+        System.out.println("ID:" + id);
+        System.out.println("Phone:" + phone);
+        System.out.println("");
+    }
 }
--- Test_Teacher.java
+++ Test_Teacher.java
@@ -1,36 +1,19 @@
 
-package OOP;
+package OOP.Parametrised_Method;
 
 public class Test_Teacher {
     public static void main(String[] args) {
-        
-       /* Teacher teacher1;  //object declare
-        teacher1 = new Teacher(); //create*/
        
-        Teacher teacher1= new Teacher();
-        teacher1.name = "Jiabul Hoq";
-        teacher1.gender = "Male";
-        teacher1.id = 141582;
-        teacher1.phone = 1842776586;
-        
-        System.out.println("Name:" + teacher1.name);
-        System.out.println("Gender:" + teacher1.gender);
-        System.out.println("ID:" + teacher1.id);
-        System.out.println("Phone:" + teacher1.phone);
-        
-        /* Teacher teacher1;  //object declare
-        teacher1 = new Teacher(); //create*/
-        System.out.println("");
-        
+    Teacher teacher1=new Teacher();
+    teacher1.setInformation("Jiabul Hoq","Male",1842776586);
+    teacher1.displayInformation();
+    
         Teacher teacher2= new Teacher();
         teacher2.name = "Amanul Islam";
         teacher2.gender = "Male";
         teacher2.id = 132485;
-        teacher2.phone = 1842886886;
-        
-        System.out.println("Name:" + teacher2.name);
-        System.out.println("Gender:" + teacher2.gender);
-        System.out.println("ID:" + teacher2.id);
-        System.out.println("Phone:" + teacher2.phone);
+        teacher2.phone = 1842887584;
+        teacher2.displayInformation();
     }
+ 
 }
